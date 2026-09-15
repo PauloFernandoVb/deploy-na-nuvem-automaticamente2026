94649654af1985ae6dff84a42223e8756ed4e5b7
Paulo Fernando Vilas Boas <168550782+PauloFernandoVb@users.noreply.github.com>
2026-09-15 20:25:23 -0300

    Fix server log message for correct status
    Updated the server log message to indicate it's running correctly.

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ function getIpAddress() {
 }
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando certo em http://localhost:${port}`);
 });
